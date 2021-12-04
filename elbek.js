2e364d3627e3ddca36adc11e0f98fd594bea7790
abdulkhaeff <elbekabdulkhaev>
2021-12-04 16:53:36 +0500

    salooooom

---
+++ elbek.js
@@ -0,0 +1,17 @@
+// // var a = "luna";
+// // var b = "slaom"
+
+// // git add .
+// // git commit -m
+// // var a = "sooo"
+// console.log(a);
+
+// if (salom) {ShadowRoot
+//     qwe
+    
+// }
+
+
+let a = 3;
+let v = 4;
+console.log(a+v);
