b26a0a1fba7f2a4beda8661cbb40a94abba237e0
abdulkhaeff <elbekabdulkhaev@gmail.com>
2022-01-08 15:21:54 +0500

    salom

--- elbek.js
+++ elbek.js
@@ -50,3 +50,27 @@
 //   }
 
 // leetcode
+
+//  sqrt =(a)=>{
+// return a**0.5
+// }
+// console.log(sqrt(4));
+// switch (key) {
+//     case value:
+// var I = 1;
+// var V = 5;
+// var X = 10;
+// var L = 50;
+// var C = 100;
+// var D = 500;
+// var M = 1000;
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+
+
